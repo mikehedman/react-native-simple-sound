0393e1f2da256f54bd60c3d59e6c00cbb2dbffe5
Mike Hedman <mikehedman@yahoo.com>
2015-11-07 19:35:03 -0800

    Changes from react-native-sound

--- RNSimpleSound/RNSimpleSound.m
+++ RNSimpleSound/RNSimpleSound.m
@@ -9,54 +9,51 @@
 #import <AVFoundation/AVFoundation.h>
 
 @implementation RNSimpleSound {
-    NSMutableArray* _playerPool;
-    BOOL _enabled;
-}
-
--(NSMutableArray*) playerPool {
-    if (!_playerPool) {
-        _playerPool = [[NSMutableArray alloc] init];
-    }
-    return _playerPool;
+  BOOL _enabled;
+  AVAudioPlayer* player;
 }
 
 -(NSURL*) soundURL:(NSString*)fileName {
-    return [[NSBundle mainBundle] URLForResource:[[fileName lastPathComponent]stringByDeletingPathExtension]
-                                   withExtension:[fileName pathExtension]];
+  return [[NSBundle mainBundle] URLForResource:[[fileName lastPathComponent]stringByDeletingPathExtension]
+                                 withExtension:[fileName pathExtension]];
 }
 
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(enable:(BOOL)enabled) {
-    _enabled = enabled;
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory: AVAudioSessionCategoryAmbient error: nil];
-    [session setActive: enabled error: nil];
+  _enabled = enabled;
+  AVAudioSession *session = [AVAudioSession sharedInstance];
+  [session setCategory: AVAudioSessionCategoryAmbient error: nil];
+  [session setActive: enabled error: nil];
 }
 
 RCT_EXPORT_METHOD(prepare:(NSString *)fileName) {
-    AVAudioPlayer* player = [[AVAudioPlayer alloc] initWithContentsOfURL:[self soundURL:fileName] error:nil];
-    [player prepareToPlay];
-    [[self playerPool] addObject:player];
+  player = [[AVAudioPlayer alloc] initWithContentsOfURL:[self soundURL:fileName] error:nil];
+  [player prepareToPlay];
 }
 
-RCT_EXPORT_METHOD(play:(NSString *)fileName) {
+RCT_EXPORT_METHOD(play) {
+  if (!_enabled || !player.url) {
+    return;
+  }
+
+  [player play];
+}
+
+RCT_EXPORT_METHOD(pause) {
+  if (!_enabled) {
+    return;
+  }
+  [player pause];
+}
+
+RCT_EXPORT_METHOD(stop) {
     if (!_enabled) {
         return;
     }
-    
-    NSURL *soundURL = [self soundURL:fileName];
-    
-    for (AVAudioPlayer* player in [[self playerPool] mutableCopy]) {
-        if (!player.playing && [player.url isEqual:soundURL]) {
-            [player play];
-            return;
-        }
-    }
-    
-    AVAudioPlayer* player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error:nil];
-    [player play];
-    [[self playerPool] addObject:player];
+    [player stop];
+    //stop does not reset the player, so force it
+    [player prepareToPlay];
 }
 
 @end
